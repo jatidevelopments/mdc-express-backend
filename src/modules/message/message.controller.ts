84c3db9f56a96d8f4b3c8a4c89fc7f633e29fc7a
jatidevelopments <janis@tirtey.de>
2024-10-28 16:21:45 +0100

    Updated flightcontrol

--- src/modules/message/message.controller.ts
+++ src/modules/message/message.controller.ts
@@ -1,8 +1,8 @@
 // message.controller.ts
 import { Request, Response } from 'express';
-import { messageCensorship, heavyCalculation, getImageAsBase64 } from './helpers';
-import { createMessage, updateConversation } from './message.service';
+import { getImageAsBase64, messageCensorship } from './helpers';
 import { SendMessageSchemaType } from './message.schema';
+import { createMessage, updateConversation } from './message.service';
 
 export const handleSendMessage = async (
   req: Request<unknown, unknown, SendMessageSchemaType>,
@@ -21,7 +21,7 @@ export const handleSendMessage = async (
       return;
     }
 
-    const calculationResult = heavyCalculation();
+    // const calculationResult = heavyCalculation();
     const newMessage = await createMessage(conversation_id, message, character_id || '');
 
     await updateConversation(conversation_id, newMessage.id);
@@ -30,7 +30,7 @@ export const handleSendMessage = async (
       'https://i.ibb.co/ThyGvSb/mdc-test-image.webp'
     );
 
-    res.status(200).json({ message: newMessage, base64Image, calculationResult });
+    res.status(200).json({ message: newMessage, base64Image });
   } catch (error) {
     console.error('Internal server error:', error);
     res.status(500).json({ error: 'Internal server error' });
